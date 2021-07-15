ab49dcc405b957f9f8b06186313e792abaf01b77
Niket Kumar <bunnyscac@outlook.com>
2021-07-15 11:44:05 +0530

    UPDATE: Added the Social Cards and Tried UPSKILL API

--- HomePage/HomePage.jsx
+++ HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import dataJson from "./data.json" 
 import { userActions } from '../_actions';
 
 class HomePage extends React.Component {
@@ -11,36 +11,6 @@ class HomePage extends React.Component {
 
     handleDeleteUser(id) {
         return (e) => this.props.deleteUser(id);
-    }
-
-    render() {
-        const { user, users } = this.props;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React!!</p>
-                <h3>All registered users:</h3>
-                {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                                {
-                                    user.deleting ? <em> - Deleting...</em>
-                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
-                                }
-                            </li>
-                        )}
-                    </ul>
-                }
-                <p>
-                    <Link to="/login">Logout</Link>
-                </p>
-            </div>
-        );
     }
 }
 
